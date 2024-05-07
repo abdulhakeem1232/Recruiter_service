8217df27d953c73d21138a1436f192d4e5f91f17
hakeem <hakeemforgit@gmail.com>
2024-05-07 20:20:36 +0530

    recruiter login signup

---
+++ src/model/recruiterModel.ts
@@ -0,0 +1,67 @@
+import mongoose,{Document,Schema} from "mongoose";
+import bcrypt from 'bcryptjs'
+
+export interface Recuiter extends Document{
+    username:string;
+    email:string;
+    mobile:number;
+    companyName:string;
+    avatar?:string;
+    password:string;
+    companyemail:string;
+    status:boolean;
+
+    matchPassword: (enteredPassword: string) => Promise<boolean>
+}
+
+const RecuiterSchema :Schema<Recuiter>=new Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    mobile:{
+        type:Number,
+    },
+    companyName:{
+        type:String,
+        required:true
+    },
+    avatar:{
+        type:String,
+        required:false
+    },
+    companyemail:{
+        type:String,
+        required:false
+    },
+    password:{
+        type:String,
+       required:true 
+    },
+    status:{
+        type:Boolean,
+        default:false
+    },
+})
+
+//pre-save password
+RecuiterSchema.pre<Recuiter>('save',async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+      }
+    
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt); 
+      next();
+    });
+
+    RecuiterSchema.methods.matchPassword=async function(eneteredpassword:string){
+    return await bcrypt.compare(eneteredpassword,this.password)
+}
+const RecuiterModel = mongoose.model<Recuiter>("Recuiter", RecuiterSchema);
+
+export default RecuiterModel;
